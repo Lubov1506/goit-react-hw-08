7e7eaf954500f53114382d8e6602d37bcbda6083
Lubov Parfonova <87125430+Lubov1506@users.noreply.github.com>
2024-05-21 15:58:15 +0300

    add slice auth

---
+++ src/redux/auth/selectors.js
@@ -0,0 +1,3 @@
+export const selectUserName = (state) => state.user.name;
+export const selectToken = (state) => state.user.token;
+export const selectIsLoggedIn = (state) => state.user.isLoggedIn;
--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -1,9 +1,50 @@
+import { createSlice } from "@reduxjs/toolkit";
+import {
+  loginThunk,
+  logoutThunk,
+  refreshUserThunk,
+  registerThunk,
+} from "./operations";
+
 const initialState = {
   user: {
-    name: null,
-    email: null,
+    name: "",
+    email: "",
   },
   token: null,
   isLoggedIn: false,
   isRefreshing: false,
 };
+
+const slice = createSlice({
+  name: "auth",
+  initialState,
+  extraReducers: (builder) => {
+    builder
+      .addCase(registerThunk.fulfilled, (state, { payload }) => {
+        state.user = payload.user;
+        state.token = payload.token;
+        state.isLoggedIn = true;
+      })
+      .addCase(loginThunk.fulfilled, (state, { payload }) => {
+        state.user = payload.user;
+        state.token = payload.token;
+        state.isLoggedIn = true;
+      })
+      .addCase(logoutThunk.fulfilled, () => {
+        return initialState;
+      })
+      .addCase(refreshUserThunk.fulfilled, (state, { payload }) => {
+        state.user = payload;
+        state.isLoggedIn = true;
+        state.isRefreshing = false;
+      })
+      .addCase(refreshUserThunk.pending, (state) => {
+        state.isRefreshing = true;
+      })
+      .addCase(refreshUserThunk.rejected, (state) => {
+        state.isRefreshing = false;
+      });
+  },
+});
+export const authReducer = slice.reducer;
