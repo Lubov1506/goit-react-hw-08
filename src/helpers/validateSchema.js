6c1438d2dc5393ff3c835c103e93a19e4e8a15d0
Lubov Parfonova <87125430+Lubov1506@users.noreply.github.com>
2024-05-22 12:27:11 +0300

    Update validateSchema.js

--- src/helpers/validateSchema.js
+++ src/helpers/validateSchema.js
@@ -1,11 +1,10 @@
-import toast from "react-hot-toast";
 import * as Yup from "yup";
 
 export const FeedbackSchema = Yup.object().shape({
   name: Yup.string()
     .matches(/^[a-zA-Zа-яА-ЯїЇіІєЄёЁґҐ\s']+$/, "Name can only contain letters")
-    .min(3, "Too short!")
-    .max(50, "Too long!")
+    .min(3, "Name too short!")
+    .max(50, "Name too long!")
     .required("Please, type your name"),
   number: Yup.string()
     .matches(/^\d{7}$/, "Number must be exactly 7 digits")
@@ -14,25 +13,25 @@ export const FeedbackSchema = Yup.object().shape({
 
 export const LoginSchema = Yup.object().shape({
   email: Yup.string()
-    .matches(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g, "Incorrect format")
+    .matches(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g, "Incorrect format email")
     .required("Type email!"),
   password: Yup.string()
     .required("Type password")
-    .min(6, "Must be not shorter 6 symbols")
-    .max(20, "Too long!"),
+    .min(6, "Password must be not shorter 6 symbols")
+    .max(20, "Password too long!"),
 });
 
 export const RegisterSchema = Yup.object().shape({
   name: Yup.string()
     .matches(/^[a-zA-Zа-яА-ЯїЇіІєЄёЁґҐ\s']+$/, "Name can only contain letters")
-    .min(3, "Too short!")
-    .max(50, "Too long!")
+    .min(3, "Name too short!")
+    .max(50, "Name too long!")
     .required("Please, type your name"),
   email: Yup.string()
-    .matches(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g, "Incorrect format")
+    .matches(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g, "Incorrect format email")
     .required("Type email!"),
   password: Yup.string()
     .required("Type password")
-    .min(6, "Must be not shorter 6 symbols")
-    .max(20, "Too long!"),
+    .min(6, "Password ust be not shorter 6 symbols")
+    .max(20, "Too long password!"),
 });
